b3978a9f0def43699b35c995c57f0dd0907d92b8
ltphat2204 <ltphat2204@gmail.com>
2024-08-06 12:17:54 +0700

    Add home screen and image button

--- App.py
+++ App.py
@@ -1,15 +1,13 @@
 import pygame
-from Screen import Screen
+from Screen import Home
 from Constant import Setting
 
 
 class Window:
     def __init__(self) -> None:
         pygame.init()
-        self.setup_attributes()
         pygame.display.set_caption("Delivery system")
 
-    def setup_attributes(self) -> None:
         self.SCREEN_HEIGHT = Setting.HEIGHT
         self.SCREEN_WIDTH = Setting.WIDTH
 
@@ -18,8 +16,10 @@ class Window:
         font_url = "./Assets/Poppins.ttf"
         self.text_font = pygame.font.Font(font_url, 40)
 
+        self.background = "./Assets/background.jpg"
+
     def run(self) -> None:
-        Screen.Screen(self.screen, self.SCREEN_HEIGHT, self.SCREEN_WIDTH, self.text_font).run()
+        Home.Home(self.screen, self.SCREEN_HEIGHT, self.SCREEN_WIDTH, self.text_font, self.background).run()
         pygame.quit()
 
 
--- Component/Button.py
+++ Component/Button.py
@@ -1,5 +1,6 @@
 import pygame
 from Component import Component
+from typing import Callable
 
 
 class Button(Component.Component):
@@ -26,3 +27,23 @@ class Button(Component.Component):
 
     def show(self, screen: pygame.Surface, x: int, y: int) -> None:
         pass
+
+
+class ImageButton(Button):
+    def __init__(self, image: str, scale: float = 1) -> None:
+        super(ImageButton, self).__init__()
+        btn_img = pygame.image.load(image)
+        btn_img = btn_img.convert_alpha()
+
+        width = btn_img.get_width()
+        height = btn_img.get_height()
+        self.image = pygame.transform.scale(btn_img, (int(width * scale), int(height * scale)))
+
+    def draw(self, screen: pygame.Surface, x: int, y: int, action: Callable) -> None:
+        rect = self.image.get_rect()
+        rect.topleft = (x - self.image.get_width() // 2, y)
+
+        if self.is_clicked(rect):
+            action()
+
+        screen.blit(self.image, (rect.x, rect.y))
