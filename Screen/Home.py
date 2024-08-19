50a6211013edf6812fc0930ff1c66b9c4b61f60e
ltphat2204 <ltphat2204@gmail.com>
2024-08-19 11:22:27 +0700

    Format Home screen

--- Screen/Home.py
+++ Screen/Home.py
@@ -8,24 +8,15 @@ from Component.Button import ImageButton
 class Home(Screen):
     def __init__(self, *args):
         super(Home, self).__init__(*args)
-        self.title = Text("Choose input to continue", self.font, Color.WHITE)
+        self.title = Text("Choose map to continue", self.font, Color.WHITE)
         self.button1 = ImageButton("./Assets/inputs/input1.png", 0.25)
         self.button2 = ImageButton("./Assets/inputs/input2.png", 0.25)
-        self.button3 = ImageButton("./Assets/inputs/input3.png", 0.25)
-        self.button4 = ImageButton("./Assets/inputs/input4.png", 0.25)
-        self.button5 = ImageButton("./Assets/inputs/input5.png", 0.25)
 
-    def handleInput(self, input: int) -> callable:
+    def handle_map(self, inp: int) -> callable:
         def handle() -> None:
-            if input == 1:
+            if inp == 1:
                 pass
-            elif input == 2:
-                pass
-            elif input == 3:
-                pass
-            elif input == 4:
-                pass
-            elif input == 5:
+            elif inp == 2:
                 pass
 
         return handle
@@ -37,12 +28,9 @@ class Home(Screen):
                     self.running = False
 
             self.draw_background()
-            self.title.show(self.screen, self.SCREEN_WIDTH // 2, 100)
+            self.title.show(self.screen, self.SCREEN_WIDTH // 2, 150)
 
-            self.button1.draw(self.screen, self.SCREEN_WIDTH // 2, 200, self.handleInput(1))
-            self.button2.draw(self.screen, self.SCREEN_WIDTH // 2, 270, self.handleInput(2))
-            self.button3.draw(self.screen, self.SCREEN_WIDTH // 2, 340, self.handleInput(3))
-            self.button4.draw(self.screen, self.SCREEN_WIDTH // 2, 410, self.handleInput(4))
-            self.button5.draw(self.screen, self.SCREEN_WIDTH // 2, 480, self.handleInput(5))
+            self.button1.draw(self.screen, self.SCREEN_WIDTH // 2, 300, self.handle_map(1))
+            self.button2.draw(self.screen, self.SCREEN_WIDTH // 2, 370, self.handle_map(2))
 
             pygame.display.update()
